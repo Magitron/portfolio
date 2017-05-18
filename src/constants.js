8f0cb03bed1d66471b5fbdf34f59d63e5948df12
Magitron <maggiecaspar@gmail.com>
2017-05-18 16:06:33 +0700

    add color variables

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const colors = {
+  saphire: '#354ECE',
+  carnation: '#FA4F5B',
+  perano: '#A3AFEC',
+  puertorico: '#3BBEA1',
+
+  // UI Colors
+  notBlack: '#242935',
+  carkBrey: '#626262',
+  concrete: '#DFE1E4',
+  lightGrey: '#F8F8F8',
+  wall: '#F9F9F9',
+  white: '#FFFFFF'
+};
